55b9bf22a02543f978f8012da680b2764393318a
yegorkotseba <yegor.kotseba@gmail.com>
2020-06-03 13:30:19 +0300

    add before class exception

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,9 +1,17 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Demo {
+
+    @BeforeClass(alwaysRun = true)
+    void prepareData() {
+        throw new NullPointerException();
+    }
 
     @Test
     void demoTest() {
         System.out.println("test started!");
     }
+
+
 }
