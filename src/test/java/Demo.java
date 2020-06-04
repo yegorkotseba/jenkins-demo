157cfdade685b262e5a2e1db6f6dd3ed14f5ca2f
yegorkotseba <yegor.kotseba@gmail.com>
2020-06-04 12:06:29 +0300

    add failed assert

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -7,20 +7,19 @@ public class Demo {
 
     @BeforeClass(alwaysRun = true)
     void prepareData() {
-        throw new SkipException("before skip");
 
     }
 
     @Test
     void demoTest() {
-//        throw new SkipException("test skip");
+        throw new SkipException("test skip");
 
     }
 
     @Test
     void demoTest1() {
         System.out.println("test1 started!");
-
+        Assert.assertEquals(1, 5);
 
     }
 
